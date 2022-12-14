ef8265595e8d4bd213ac887b7751cd43f0775b36
Mpho Kutoane <mkutoane@singular.co.za>
2022-12-14 14:33:39 +0200

    MySQL Advanced

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,4 @@
+SELECT band_name, ifnull(split, 2022)-ifnull(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC
---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE TRIGGER new_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+	SET NEW.valid_email = 0;
+    END IF;
+END$$
+DELIMITER ;$$
