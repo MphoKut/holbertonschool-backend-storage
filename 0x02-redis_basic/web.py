d81e661ff9bd235b1c83f676caf37ffee9a434f8
Mpho Kutoane <mkutoane@singular.co.za>
2022-12-22 14:53:05 +0200

    Redis Basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+""" Implementing an expiring web cache and tracker
+    obtain the HTML content of a particular URL and returns it """
 import redis
 import requests
 r = redis.Redis()
@@ -6,6 +8,9 @@ count = 0
 
 
 def get_page(url: str) -> str:
+    """ track how many times a particular URL was accessed in the key
+        "count:{url}"
+        and cache the result with an expiration time of 10 seconds """
     r.set(f"cached:{url}", count)
     resp = requests.get(url)
     r.incr(f"count:{url}")
